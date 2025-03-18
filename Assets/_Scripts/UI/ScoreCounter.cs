4d33dd877cc7c290cd5affdb09bbbdc156a1fc86
Jaidyn Gordon-Mason <jaidynrocks01@gmail.com>
2025-03-17 19:08:08 -0700

    Implemented all logic for counter

--- Assets/_Scripts/UI/ScoreCounter.cs
+++ Assets/_Scripts/UI/ScoreCounter.cs
@@ -1,16 +1,40 @@
+using System.Collections;
+using DG.Tweening;
+using TMPro;
 using UnityEngine;
 
 public class ScoreCounter : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeField] private TextMeshProUGUI current;
+    [SerializeField] private TextMeshProUGUI toUpdate;
+    [SerializeField] private Transform scoreTextContainer;
+    [SerializeField] private float duration;
+    [SerializeField] private Ease animationCurve;
+
+    private float containerInitPosition;
+    private float moveAmount;
+
+    private void Start()
     {
-        
+        Canvas.ForceUpdateCanvases();
+        current.SetText("0");
+        toUpdate.SetText("0");
+        containerInitPosition = scoreTextContainer.localPosition.y;
+        moveAmount = current.rectTransform.rect.height;
     }
 
-    // Update is called once per frame
-    void Update()
+    public void UpdateScore(int score)
     {
-        
+        toUpdate.SetText($"{score}");
+        scoreTextContainer.DOLocalMoveY(containerInitPosition + moveAmount, duration).SetEase(animationCurve);
+        StartCoroutine(ResetCoinCounter(score));
+    }
+
+    private IEnumerator ResetCoinCounter(int score)
+    {
+        yield return new WaitForSeconds(duration);
+        current.SetText($"{score}");
+        Vector3 localPosition = scoreTextContainer.localPosition;
+        scoreTextContainer.localPosition = new Vector3(localPosition.x, containerInitPosition, localPosition.z);
     }
 }
